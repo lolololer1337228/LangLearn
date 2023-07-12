2967f4f8bddc9845bf1ca660744336be54e421f0
surkinasasha <surkinaalexandra9@gmail.com>
2023-07-12 12:15:07 +0300

    kartochki ochen' chernovie

---
+++ card.py
@@ -0,0 +1,31 @@
+class Card:
+    def __init__(self):
+        self.__card = ('','')
+        self.__word_rate_1:double = 0
+        self.__word_rate_2:double = 0
+        self.__word_rate_3:double = 0
+    def get_rate_1(self):
+        return __word_rate_1
+    def get_rate_2(self):
+        return __word_rate_2
+    def get_rate_3(self):
+        return __word_rate_3
+    def edit_card(self, __card, __word_rate_1, __word_rate_2, __word_rate_3):
+        self.card[0] = str(input())
+        self.card[1] = str(input())
+        self.word_rate_1 = 0
+        self.word_rate_2 = 0
+        self.word_rate_3 = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
