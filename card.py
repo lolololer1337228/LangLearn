f5fe513b7df6d851039c1b2029d1b38e3363f45e
surkinasasha <surkinaalexandra9@gmail.com>
2023-07-13 12:56:52 +0300

    kartochki ochen' chernovie1

--- card.py
+++ card.py
@@ -1,31 +1,16 @@
 class Card:
-    def __init__(self):
-        self.__card = ('','')
-        self.__word_rate_1:double = 0
-        self.__word_rate_2:double = 0
-        self.__word_rate_3:double = 0
-    def get_rate_1(self):
-        return __word_rate_1
-    def get_rate_2(self):
-        return __word_rate_2
-    def get_rate_3(self):
-        return __word_rate_3
-    def edit_card(self, __card, __word_rate_1, __word_rate_2, __word_rate_3):
-        self.card[0] = str(input())
-        self.card[1] = str(input())
-        self.word_rate_1 = 0
-        self.word_rate_2 = 0
-        self.word_rate_3 = 0
+    def __init__(self) -> None:
+        self.__card = ['', '']
+        self.__word_rates = [0, 0, 0]
 
+    def get_card_content(self) -> list:
+        return self.__card
 
+    def get_rates(self) -> list:
+        return self.__word_rates
 
-
-
-
-
-
-
-
-
-
+    def edit_card(self, word:str, translation:str) -> None:
+        self.__card[0] = word
+        self.__card[1] = translation
+        self.__word_rates = [0, 0, 0]
 
