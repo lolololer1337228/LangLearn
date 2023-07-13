36873b0f3fe55b0a6397e3a805e2e18c63f6dc7b
Furezuu <nikitavoronin501@gmail.com>
2023-07-13 10:32:50 +0300

    Перед слиянием

--- mode_interface.py
+++ mode_interface.py
@@ -1,9 +1,9 @@
-class ModeInterface:
+class ModeStrategy:
     def __init__(flag, card_kit):
         self.__flag = flag
         self.__card_kit = card_kit
 
     def create_sequence(self):
-        return self.__card_kit
+        return sorted(self.__card_kit, key=lambda x: -x.rate)
 
-
+    def 
